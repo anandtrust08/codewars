7e603eb3862b5c17094a088d3f5169a1a75a5d67
pxai <pello_altadill@cuatrovientos.org>
2018-03-02 11:06:15 +0100

    Done

--- src/io/pello/codewars/matrixsquareup/MatrixSquareUp.java
+++ src/io/pello/codewars/matrixsquareup/MatrixSquareUp.java
@@ -3,6 +3,12 @@ package io.pello.codewars.matrixsquareup;
 public class MatrixSquareUp {
 
 	  public static String[][] matrixSquareUp(int b) {
-	    return null;
+		  String [][]result = new String[b][b];
+		  
+		  for (int i=0;i<b;i++)
+			  for (int j=b-1;j>=0;j--)
+				  result[i][j] = (j+1+i>=b)?(b-j)+"":"x";
+		  
+	    return result;
 	    }
 }
