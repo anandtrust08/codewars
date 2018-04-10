d7e6fc3db16dc35046babe3cea953175f348b0cc
pxai <pello_altadill@cuatrovientos.org>
2018-04-10 20:38:54 +0200

    Improvements

--- src/io/pello/codewars/dirreduction/DirReduction.java
+++ src/io/pello/codewars/dirreduction/DirReduction.java
@@ -1,7 +1,7 @@
 package io.pello.codewars.dirreduction;
 
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DirReduction {
@@ -13,22 +13,20 @@ public class DirReduction {
         for (int i = 0;i< arr.length;i++)
             directions.add(arr[i]);
         
-        System.out.println(directions);
         for (int i=0;i<directions.size()-1;i++) {
             if ( (directions.get(i).equals("NORTH") && directions.get(i+1).equals("SOUTH")) ||
                 (directions.get(i).equals("SOUTH") && directions.get(i+1).equals("NORTH")) || 
                 (directions.get(i).equals("EAST") && directions.get(i+1).equals("WEST")) ||
                 (directions.get(i).equals("WEST") && directions.get(i+1).equals("EAST"))) {
-                System.out.println(directions);      
-                System.out.println("OK, removing: "+i+": " + directions.get(i)+ ", "+directions.get(i+1));
+
                 directions.remove(i);
                 directions.remove(i);
-                System.out.println(directions);
+
                 i = (i>1)?i-2:-1;
             } 
 
         }
-        System.out.println(directions);
+  
         String[] result = new String[directions.size()];
         
         for (int i =0;i< result.length;i++)
@@ -37,3 +35,4 @@ public class DirReduction {
         return result;
     }
 }
+
