e1dc283d85188784efa91da13a9f26368a36532c
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-04-22 17:28:09 +0200

    Working good

--- src/io/pello/codewars/stripcomments/StripComments.java
+++ src/io/pello/codewars/stripcomments/StripComments.java
@@ -2,32 +2,31 @@ package io.pello.codewars.stripcomments;
 
 public class StripComments {
 
-        public static String stripComments(String text, String[] commentSymbols) {
-            String result = "";
-            boolean comment = false;
+    public static String stripComments(String text, String[] commentSymbols) {
+        String result = "";
+        boolean comment = false;
 
-            for (int i = 0; i < text.length(); i++) {
-                if (comment) {
+        for (int i = 0; i < text.length(); i++) {
+            if (comment) {
+                if (text.charAt(i) == '\n') {
+                    comment = false;
+                }
+            }
+
+            if (!comment) {
+                if (java.util.Arrays.asList(commentSymbols)
+                    .indexOf(text.charAt(i) + "") != -1) {
+                    comment = true;
+                } else {
                     if (text.charAt(i) == '\n') {
-                        comment = false;
-                    }
-                }
-
-                if (!comment) {
-                    if (java.util.Arrays.asList(commentSymbols)
-                        .indexOf(text.charAt(i) + "") != -1) {
-                        comment = true;
+                        result = result.replaceAll("[ ]+$", "") + "\n";
                     } else {
-                        if (text.charAt(i) == '\n') {
-                            result = result.replaceAll("[ ]+$", "") + "\n";
-                        } else {
-                            result += text.charAt(i);
-                        }
+                        result += text.charAt(i);
                     }
                 }
             }
-            return result.replaceAll("[ ]+$", ""); // result.substring(0, result.length() - 1);
-            }
-
+        }
+        return result.replaceAll("[ ]+$", ""); // result.substring(0, result.length() - 1);
+    }
 
 }
---
+++ src/io/pello/codewars/timeformatter/TimeFormatter.java
@@ -0,0 +1,43 @@
+package io.pello.codewars.timeformatter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TimeFormatter {
+    public static String formatDuration(int seconds) {
+        if (seconds == 0)
+            return "now";
+
+        List<Integer> values = new ArrayList<Integer>();
+        List<String> listNames = new ArrayList<String>();
+
+        int division[] = new int[] { 31536000, 86400, 3600, 60, 1 };
+        String names[] = new String[] { "year", "day", "hour", "minute", "second" };
+
+        String result = "";
+        int remain = seconds;
+
+        for (int i = 0; i < division.length; i++) {
+            if (remain >= division[i]) {
+                values.add(remain / division[i]);
+                listNames.add(names[i]);
+                remain %= division[i];
+            }
+        }
+
+        System.out.println(values);
+        if (values.size() == 1) {
+               return values.get(0) + " " + listNames.get(0) + (values.get(0) > 1 ? "s" : "");
+        } else {
+            int i = 0;
+            for (i = 0; i < values.size()-1; i++) {
+                String suffix = values.get(i) > 1 ? "s" : "";
+                result += values.get(i) + " " + listNames.get(i) + suffix + ", ";
+            }
+            result = result.substring(0,result.length()-2)+ " and " + values.get(i) + " " + listNames.get(i) + (values.get(i) > 1 ? "s" : "");
+        }
+
+        return result;
+    }
+}
