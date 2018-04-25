a844083284dd4b3f66a4885237deb50421f781df
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-04-25 23:27:35 +0200

    First approach, Hashtable with three stacks... ITS WRONG!

---
+++ src/io/pello/codewars/bracechecker/BraceChecker.java
@@ -0,0 +1,20 @@
+package io.pello.codewars.bracechecker;
+
+import java.util.Hashtable;
+import java.util.Stack;
+
+public class BraceChecker {
+
+
+    public boolean isValid(String braces) {
+       boolean result = false;
+       Hashtable<String,Stack<String>> code =   new Hashtable<String,Stack<String>>();
+        
+        for (int i = 0; i<braces.length();i++) {
+            
+
+        }
+        
+       return code.size() == 0;
+      }
+}
---
+++ src/io/pello/codewars/bracechecker/BraceCheckerTest.java
@@ -0,0 +1,20 @@
+package io.pello.codewars.bracechecker;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BraceCheckerTest {
+
+  private BraceChecker checker = new BraceChecker();
+
+	@Test
+	public void testValid() {
+    assertEquals(true, checker.isValid("()"));
+	}
+  
+  @Test
+	public void testInvalid() {
+    assertEquals(false, checker.isValid("[(])"));
+	}
+
+}
