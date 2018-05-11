32f47f31ea24a5c3aa1916518c02aab873c40001
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-05-11 23:57:46 +0200

    Working!!

--- src/io/pello/codewars/pascaltriangle/PascalTriangleTest.java
+++ src/io/pello/codewars/pascaltriangle/PascalTriangleTest.java
@@ -13,8 +13,15 @@ class PascalTriangleTest {
     private List<List<Integer>> generateResult () {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         int [][] numbers = new int[][] { {1}, {1,1} ,{1,2,1} , {1,3,3,1}, {1,4,6,4,1}};
-        for (int i = 0; i< numbers.length; i++)
-            result.add(Arrays.asList(numbers[i])));
+        
+        for (int i = 0; i< numbers.length; i++) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j<numbers[i].length;j++) {
+                row.add(numbers[i][j]);
+            }
+            result.add(row);
+        }
+        
         return result;
     }
     
