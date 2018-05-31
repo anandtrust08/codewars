df56e3605ab8f97bce8cc3fd6e8f16839f855acf
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-05-31 12:55:05 +0200

    Minor changes

--- src/io/pello/codewars/gasstation/GasStation.java
+++ src/io/pello/codewars/gasstation/GasStation.java
@@ -2,18 +2,18 @@ package io.pello.codewars.gasstation;
 
 public class GasStation {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-      
-      int start = 0;
-      int total = 0;
-      int tank = 0;
 
-      for (int i = 0; i < gas.length; i++)
-        if ((tank = tank + gas[i] - cost[i]) < 0) {
-          start = i + 1;
-          total += tank;
-          tank = 0;
-        }
-      
-      return (total + tank < 0) ? -1 : start;
+        int start = 0;
+        int total = 0;
+        int tank = 0;
+
+        for (int i = 0; i < gas.length; i++)
+            if ((tank = tank + gas[i] - cost[i]) < 0) {
+                start = i + 1;
+                total += tank;
+                tank = 0;
+            }
+
+        return (total + tank < 0) ? -1 : start;
     }
 }
