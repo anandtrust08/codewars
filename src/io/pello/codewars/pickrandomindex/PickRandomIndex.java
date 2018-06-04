efd3b3a5f6d70e4a54f79fab4e67294dd068b063
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-06-04 09:49:46 +0200

    Ok, now works

--- src/io/pello/codewars/pickrandomindex/PickRandomIndex.java
+++ src/io/pello/codewars/pickrandomindex/PickRandomIndex.java
@@ -1,19 +1,18 @@
 package io.pello.codewars.pickrandomindex;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class PickRandomIndex {
     public int pickRandom(int [] numbers, int target) {
         Random random = new Random();
-        int result = -1;
-        int count = 0;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
         for (int i = 0; i < numbers.length; i++) {
           if (numbers[i] != target) { continue; }
-          count++;
-          if (random.nextInt(count) == 0)
-            result = i;
+          list.add(i);
         }
 
-        return result;
+        return list.size() == 0?-1:list.get(random.nextInt(list.size()));
       }
 }
--- src/io/pello/codewars/pickrandomindex/PickRandomIndexTest.java
+++ src/io/pello/codewars/pickrandomindex/PickRandomIndexTest.java
@@ -17,15 +17,12 @@ class PickRandomIndexTest {
     @Test
     void testPick() {
         int [] array = new int[] {2,4,4,2,3,4,2,1,2,3};
-        int number = 3;
-        int result = pickRandomIndex.pickRandom(array, number);
-        assertTrue(result == 9);
-  /*    for (int i = 0;i< 100;i++) {
+        int number = 3; 
+        
+      for (int i = 0;i< 100;i++) {
             int result = pickRandomIndex.pickRandom(array, number);
-            assertTrue(result == 9);
-            result = pickRandomIndex.pickRandom(array, 1);
-            assertTrue(result == 7);
-        }*/
+            assertTrue(result == 9 || result == 4);
+        }
     }
 
 }
