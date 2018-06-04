ad53637ccb129509f96e3715967d3d8ffa13e599
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-06-04 09:57:46 +0200

    Alternate solution

--- src/io/pello/codewars/pickrandomindex/PickRandomIndex.java
+++ src/io/pello/codewars/pickrandomindex/PickRandomIndex.java
@@ -4,15 +4,32 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class PickRandomIndex {
-    public int pickRandom(int [] numbers, int target) {
+    public int pickRandom(int[] numbers, int target) {
         Random random = new Random();
         ArrayList<Integer> list = new ArrayList<Integer>();
-        
+
         for (int i = 0; i < numbers.length; i++) {
-          if (numbers[i] != target) { continue; }
-          list.add(i);
+            if (numbers[i] != target) {
+                continue;
+            }
+            list.add(i);
         }
 
-        return list.size() == 0?-1:list.get(random.nextInt(list.size()));
-      }
+        return list.size() == 0 ? -1 : list.get(random.nextInt(list.size()));
+
+    }
+
+    public int pickRandomAlt(int[] numbers, int target) {
+        Random random = new Random();
+        int result = -1;
+        int count = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] != target)
+                continue;
+            if (random.nextInt(++count) == 0)
+                result = i;
+        }
+
+        return result;
+    }
 }
--- src/io/pello/codewars/pickrandomindex/PickRandomIndexTest.java
+++ src/io/pello/codewars/pickrandomindex/PickRandomIndexTest.java
@@ -25,4 +25,14 @@ class PickRandomIndexTest {
         }
     }
 
+    @Test
+    void testPickAlt() {
+        int [] array = new int[] {2,4,4,2,3,4,2,1,2,3};
+        int number = 3; 
+        
+      for (int i = 0;i< 100;i++) {
+            int result = pickRandomIndex.pickRandomAlt(array, number);
+            assertTrue(result == 9 || result == 4);
+        }
+    }
 }
